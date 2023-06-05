61f3d802052eeeb814d1d8b6599f646bf1ef772d
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-05 15:30:35 +0200

    trigger rebuild when autoconf.h changes

--- cmake/top.cmake
+++ cmake/top.cmake
@@ -2,12 +2,13 @@ set(PROJECT_ROOT ${CMAKE_SOURCE_DIR})
 set(KCONFIG_ROOT ${CMAKE_SOURCE_DIR}/Kconfig)
 set(RL_CONFIG_DIR ${CMAKE_SOURCE_DIR}/configs)
 set(AUTOCONF_DIR ${CMAKE_CURRENT_BINARY_DIR}/kconfig/include/generated)
+set(AUTOCONF_FILE ${CMAKE_CURRENT_BINARY_DIR}/include/generated/autoconf.h)
 
 # Re-configure (Re-execute all CMakeLists.txt code) when autoconf.h changes
 set_property(
   DIRECTORY
   APPEND
-  PROPERTY CMAKE_CONFIGURE_DEPENDS ${AUTOCONF_H})
+  PROPERTY CMAKE_CONFIGURE_DEPENDS ${AUTOCONF_FILE})
 
 include(cmake/extensions.cmake)
 include(cmake/python.cmake)
