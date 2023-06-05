74b33d650f8df3e2fd0a2739c9973d40b9a61323
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-05 15:55:58 +0200

    foxed autoconf file path

--- cmake/top.cmake
+++ cmake/top.cmake
@@ -1,7 +1,7 @@
 set(PROJECT_ROOT ${CMAKE_SOURCE_DIR})
 set(KCONFIG_ROOT ${CMAKE_SOURCE_DIR}/Kconfig)
 set(RL_CONFIG_DIR ${CMAKE_SOURCE_DIR}/configs)
-set(AUTOCONF_DIR ${CMAKE_CURRENT_BINARY_DIR}/kconfig/include/generated)
+set(AUTOCONF_DIR ${CMAKE_CURRENT_BINARY_DIR}/include/generated)
 set(AUTOCONF_FILE ${CMAKE_CURRENT_BINARY_DIR}/include/generated/autoconf.h)
 
 # Re-configure (Re-execute all CMakeLists.txt code) when autoconf.h changes
