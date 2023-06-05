bf1634424357f39fb473ecc6325f77f685620f86
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-05 15:56:11 +0200

    added autoconf.h header

--- example/app/src/main.cpp
+++ example/app/src/main.cpp
@@ -1,9 +1,14 @@
+#include "autoconf.h"
+
 #ifdef CONFIG_MODULE_1
 #include "module_1.hpp"
 #elif CONFIG_MODULE_2
-#include "module_1.hpp"
+#include "module_2.hpp"
 #endif
 
 #include <iostream>
 
-int main() { std::cout << "App 1"; }
+int main() {
+  std::cout << "App 1" << '\n';
+  Module2 obj;
+}
