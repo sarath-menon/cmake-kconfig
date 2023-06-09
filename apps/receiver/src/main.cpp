b06dd0685414391b9ac46b044f1eea94423ae4d8
sarath-menon <sarathmenon.downey@gmail.com>
2023-06-09 10:07:34 +0200

    code cleanup

--- apps/receiver/src/main.cpp
+++ apps/receiver/src/main.cpp
@@ -20,9 +20,6 @@ void receive_data(int serial_port) {
   uint8_t readBuffer[64]{};
   CRTPPacket packet{};
 
-  // Read bytes. The behaviour of read() (e.g. does it block?,
-  // how long does it block for?) depends on the configuration
-  // settings above, specifically VMIN and VTIME
   int num_bytes{};
 
   for (;;) {
@@ -31,17 +28,11 @@ void receive_data(int serial_port) {
       num_bytes = read(serial_port, &readBuffer, sizeof(readBuffer));
     }
 
-    // n is the number of bytes read. n may be 0 if no bytes were received,and
-    // can also be -1 to signal an error.
     if (num_bytes < 0) {
       std::lock_guard<std::mutex> lock(io_mutex);
       printf("Error reading: %s", strerror(errno));
       // return 1;
     }
-
-    // Here we assume we received ASCII data, but you might be sending raw bytes
-    // (in that case, don't try and print it to the screen like this!)
-    // printf("Read %i bytes. Received message: %s", num_bytes, read_buf);
 
     memcpy(&packet.data, &readBuffer[1], sizeof(packet.data));
     {
@@ -111,43 +102,10 @@ int main() {
     return 1;
   }
 
-  // // Write to serial port
-  // unsigned char msg[] = {'H', 'e', 'l', 'l', 'o', '\r'};
-  // write(serial_port, msg, sizeof(msg));
-
-  // // Normally you wouldn't do this memset() call, but since we will just
-  // receive
-  // // ASCII data for this example, we'll set everything to 0 so we can
-  // // call printf() easily.
-  // memset(&read_buf, '\0', sizeof(read_buf));
-
   // Allocate memory for send buffer, set size according to your needs
   const std::uint8_t sendBuffer[64] = {"selva\n"};
 
   for (;;) {
-
-    // // Read bytes. The behaviour of read() (e.g. does it block?,
-    // // how long does it block for?) depends on the configuration
-    // // settings above, specifically VMIN and VTIME
-    // int num_bytes = read(serial_port, &readBuffer, sizeof(readBuffer));
-
-    // // n is the number of bytes read. n may be 0 if no bytes were
-    // received,and
-    // // can also be -1 to signal an error.
-    // if (num_bytes < 0) {
-    //   printf("Error reading: %s", strerror(errno));
-    //   return 1;
-    // }
-
-    // // Here we assume we received ASCII data, but you might be sending raw
-    // bytes
-    // // (in that case, don't try and print it to the screen like this!)
-    // // printf("Read %i bytes. Received message: %s", num_bytes, read_buf);
-
-    // memcpy(&packet.data, &readBuffer[1], sizeof(packet.data));
-    // std::cout << "Data:" << unsigned(packet.data[1]) << '\n';
-
-    // std::this_thread::sleep_for(1ms);
 
     {
       std::lock_guard<std::mutex> lock(io_mutex);
